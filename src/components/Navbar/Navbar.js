70fb0d4af9905b1d8ee508d055e70e84d97d52f8
ZKeiLin <zlin2016@uw.edu>
2020-06-27 13:08:50 -0700

    change class to className

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,34 +1,46 @@
-import React from 'react';
-import {NavLink} from 'react-router-dom'
-import './Navbar.css'
-
-
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
 
 const navbar = () => {
-    return (
-        <div>
-            <nav class="navbar navbar-light">
-                <NavLink className="navbar-brand" exact to="/">VizView</NavLink>
-               
-                <div class="justify-content-end" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                        <NavLink className="inactive" to="/" exact activeStyle={
-                            {color: '#E84D28', textDecoration: "underline"}
-                        }>Home</NavLink>
-                        <NavLink className="inactive" to="/work" exact activeStyle={
-                            {color: '#E84D28', textDecoration: "underline"}
-                        }>Work</NavLink>
-                        <NavLink className="inactive" to="/about" exact activeStyle={
-                            {color: '#E84D28', textDecoration: "underline"}
-                        }>About</NavLink>
-                        
-                    </div>
-                </div>
-            </nav>
+  return (
+    <div>
+      <nav className="navbar navbar-light">
+        <NavLink className="navbar-brand" exact to="/">
+          VizView
+        </NavLink>
+
+        <div className="justify-content-end" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <NavLink
+              className="inactive"
+              to="/"
+              exact
+              activeStyle={{ color: "#E84D28", textDecoration: "underline" }}
+            >
+              Home
+            </NavLink>
+            <NavLink
+              className="inactive"
+              to="/work"
+              exact
+              activeStyle={{ color: "#E84D28", textDecoration: "underline" }}
+            >
+              Work
+            </NavLink>
+            <NavLink
+              className="inactive"
+              to="/about"
+              exact
+              activeStyle={{ color: "#E84D28", textDecoration: "underline" }}
+            >
+              About
+            </NavLink>
+          </div>
         </div>
-
-    )
-
-}
+      </nav>
+    </div>
+  );
+};
 
 export default navbar;
