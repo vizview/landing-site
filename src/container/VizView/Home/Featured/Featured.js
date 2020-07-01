01a7243a1f42eb7bf3a7421e91abf210e1ccf59d
Yiran Ni <yiran@yirans-mbp.myfiosgateway.com>
2020-07-01 00:31:48 -0400

    update home page

--- src/container/VizView/Home/Featured/Featured.js
+++ src/container/VizView/Home/Featured/Featured.js
@@ -1,20 +1,43 @@
 import React from "react";
 import "./Featured.css";
-
+import cereal from "../../img/thumbnails/cereal.png"
 const featured = () => {
   return (
     <div className="featured">
       <h2>Featured Projects</h2>
+      <div className="projects-sum">
+        
+        <div className="project-intro">
+        <img src={cereal}/>
+        <h3>What’s in your cereal?</h3>
+        <p>Come discover what is actually in your cereal. Play with cereal data!</p>
+        </div>
+        <div className="project-intro">
+        <img src={cereal}/>
+        <h3>What’s in your cereal?</h3>
+        <p>Come discover what is actually in your cereal. Play with cereal data!</p>
+        </div>
+        <div className="project-intro">
+        <img src={cereal}/>
+        <h3>What’s in your cereal?</h3>
+        <p>Come discover what is actually in your cereal. Play with cereal data!</p>
+        </div>
+        
+        
+      </div>
       <div className="subscribe">
         <p>Subscribe our mailing list to view the most updated projects!</p>
         <div className="sub-input">
         <input
-          id="contact-email"
+          id="contact-email-sm"
           type="email"
           placeholder="Email"
           aria-label="Email"
-        ></input>
-        <input type="submit" value="Subscribe" id="subsribe"></input>
+          required="required"
+        >
+          </input>
+        
+        <input type="submit" value="Subscribe" id="subsribe-sm"></input>
         </div>
        
       </div>
--- src/container/VizView/Home/Team/Team.js
+++ src/container/VizView/Home/Team/Team.js
@@ -1,11 +1,40 @@
 import React from "react";
 import "./Team.css";
+import red from "../../img/team/red.png"
 
 const team = () => {
   return (
     <div className="team">
         <h2>Our Team</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex </p>
+        <div className="members">
+            <div className="member">
+                <img className="profile" src={red}/>
+                <h3 className="mem-name">Alice Li</h3>
+                <h4 className="title">UX DEISGNER</h4>
+            </div>
+            <div className=" member">
+                <img className="profile" src={red}/>
+                <h3 className="mem-name">Grace Zhu</h3>
+                <h4 className="title">UX DEISGNER</h4>
+            </div>
+            <div className="member">
+                <img className="profile" src={red}/>
+                <h3 className="mem-name">Yiran Ni</h3>
+                <h4 className="title">SOFTWARE DEVELOPER</h4>
+                
+            </div>
+            <div className="member">
+                <img className="profile" src={red}/>
+                <h3 className="mem-name">Zhitao Zheng</h3>
+                <h4 className="title">PRODUCT MANAGER</h4>
+            </div>
+            <div className=" member">
+                <img className="profile" src={red}/>
+                <h3 className="mem-name">Zhiqi Lin</h3>
+                <h4 className="title">SOFTWARE DEVELOPER</h4>
+            </div>
+        </div>
     </div>
   );
 };
