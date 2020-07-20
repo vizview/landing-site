1214b951c8046692fa3a620fefd52e44dcbe6418
ZKeiLin <zlin2016@uw.edu>
2020-07-20 15:41:25 -0700

    fix hamburger navbar collapse bug

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,85 +3,85 @@ import { NavLink } from "react-router-dom";
 import hamburger from "../../container/VizView/img/icons/hamburger.svg";
 import "./Navbar.css";
 
-export default class navbar extends React.Component{
+export default class navbar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       isClicked: false,
     };
-    this.toggleDropdownState =  this.toggleDropdownState.bind(this)
+    this.toggleDropdownState = this.toggleDropdownState.bind(this);
   }
 
   toggleDropdownState() {
     const currentState = this.state.isClicked;
     this.setState({
-      isClicked: !currentState
-    })
-
-    console.log(currentState)
+      isClicked: !currentState,
+    });
   }
 
   render() {
     return (
-      
-        <div>
-          <nav className="navbar navbar-light">
-            <NavLink className="navbar-brand" exact to="/">
-              VizView
+      <div>
+        <nav className="navbar navbar-light">
+          <NavLink className="navbar-brand" exact to="/">
+            VizView
           </NavLink>
 
-            <div className=" justify-content-end" id="navbarNavAltMarkup">
-              <div className="navbar-nav web">
-                <NavLink className="inactive" to="/" exact>
-                  Home
+          <div className=" justify-content-end" id="navbarNavAltMarkup">
+            <div className="navbar-nav web">
+              <NavLink className="inactive" to="/" exact>
+                Home
               </NavLink>
-                <NavLink className="inactive" to="/work" exact>
-                  Work
+              <NavLink className="inactive" to="/work" exact>
+                Work
               </NavLink>
-                <NavLink className="inactive" to="/about" exact>
-                  About
+              <NavLink className="inactive" to="/about" exact>
+                About
               </NavLink>
-                <NavLink className="inactive" to="/contact" exact>
-                  Contact
+              <NavLink className="inactive" to="/contact" exact>
+                Contact
               </NavLink>
-              </div>
-              <div className="navbar-nav mobile" >
-                <div className={`hamburger  ${this.state.isClicked ? "active": "inactive"}` }
-                  onClick={this.toggleDropdownState}
-                >
-  
-                  <img src={hamburger} />
-                </div>
+            </div>
+            <div className="navbar-nav mobile">
+              <div
+                className={`hamburger  ${
+                  this.state.isClicked ? "active" : "inactive"
+                }`}
+                onClick={this.toggleDropdownState}
+              >
+                <img src={hamburger} />
               </div>
             </div>
-          </nav>
-        
-        <div className={`nav-dropdown  ${this.state.isClicked ? "active": "inactive"}`}>
-          <div className="dropdown-item">
+          </div>
+        </nav>
+
+        <div
+          className={`nav-dropdown  ${
+            this.state.isClicked ? "active" : "inactive"
+          }`}
+        >
+          <div className="dropdown-item" onClick={this.toggleDropdownState}>
             <NavLink className="inactive" to="/" exact>
               Home
-              </NavLink>
+            </NavLink>
           </div>
-          <div className="dropdown-item">
+          <div className="dropdown-item" onClick={this.toggleDropdownState}>
             <NavLink className="inactive" to="/work" exact>
               Work
-              </NavLink>
+            </NavLink>
           </div>
-          <div className="dropdown-item">
+          <div className="dropdown-item" onClick={this.toggleDropdownState}>
             <NavLink className="inactive" to="/about" exact>
               About
-              </NavLink>
+            </NavLink>
           </div>
-          <div className="dropdown-item">
+          <div className="dropdown-item" onClick={this.toggleDropdownState}>
             <NavLink className="inactive" to="/contact" exact>
               Contact
-              </NavLink>
+            </NavLink>
           </div>
         </div>
-      </div >
-    )
+      </div>
+    );
   }
-  
-  
-};
-
+}
